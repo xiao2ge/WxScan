513057e60e8cd9c0fcc4a45a40dfaeef6315369f
xiao2ge <1038133569@qq.com>
2023-04-30 14:17:39 +0800

    Update MainActivity.java

--- app/src/main/java/com/eg/lr/wxscan/MainActivity.java
+++ app/src/main/java/com/eg/lr/wxscan/MainActivity.java
@@ -1,21 +1,21 @@
-package com.eg.lr.wxscan;
+// package com.eg.lr.wxscan;
 
-import androidx.appcompat.app.AppCompatActivity;
+// import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
+// import android.annotation.SuppressLint;
+// import android.content.Intent;
+// import android.os.Bundle;
 
-import com.eg.lr.wxscan.qr.WxQrUtil;
+// import com.eg.lr.wxscan.qr.WxQrUtil;
 
-public class MainActivity extends AppCompatActivity {
+// public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//     @SuppressLint("MissingInflatedId")
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn).setOnClickListener(view -> WxQrUtil.INSTANCE.toScan(MainActivity.this));
-    }
-}
+//         findViewById(R.id.btn).setOnClickListener(view -> WxQrUtil.INSTANCE.toScan(MainActivity.this));
+//     }
+// }
